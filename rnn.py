bfa535bc7450d8d6ef6e1d3691a41ed4b65b0f6e
Abhay D <a.deshpande012@gmail.com>
2017-09-29 16:29:47 -0700

    Trained on shakespeare

--- rnn.py
+++ rnn.py
@@ -9,26 +9,28 @@ from keras import backend as K
 K.set_learning_phase(1)
 
 # Get raw data and make lowercase
-raw_text = open('wonderland.txt').read()
+raw_text = open('shakespeare.txt').read()
 raw_text = raw_text.lower()
 
 # Assemble a dict of all unique characters to a corresponding number
 chars = sorted(list(set(raw_text)))
 char_mapping = dict((c,i) for i, c in enumerate(chars))
-joblib.dump(char_mapping, 'char_mapping.sav')
+joblib.dump(char_mapping, 'shakespeare/char_mapping.sav')
+
+# Get number of unique characters, and number of total characters
+n_chars = len(raw_text)
+n_vocab = len(chars)
 
 # Create One Hot Encoder, and fit on char mapping
 ohe = OneHotEncoder(categorical_features=[0])
 ohe.fit(np.reshape([char_mapping[char] for char in chars], (len(chars), 1)))
+joblib.dump(ohe, 'shakespeare/ohe.sav')
 
 # Encode raw_text into One-Hot-Encoding
 encoded_text = [char_mapping[char] for char in raw_text]
 encoded_text = np.reshape(encoded_text, (len(raw_text), 1))
 encoded_text = ohe.transform(encoded_text).toarray()
-
-# Get number of unique characters, and number of total characters
-n_chars = len(raw_text)
-n_vocab = len(chars)
+del raw_text
 
 # Create sequences of 100 and their corresponding output
 seq_length = 100
@@ -58,7 +60,7 @@ model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
 
 # Fit and save RNN
 model.fit(x_train, y_train, epochs=50, batch_size=64)
-model.save('model.h5')
+model.save('shakespeare/model.h5')
 
 # Import libraries
 from keras.models import load_model
@@ -70,10 +72,10 @@ from keras import backend as K
 K.set_learning_phase(0)
 
 # Load model, char mapping, encoder, and raw text from disk
-model = load_model('model.h5')
-char_mapping = joblib.load('char_mapping.sav')
-ohe = joblib.load('ohe.sav')
-raw_text = open('wonderland.txt').read().lower()
+model = load_model('shakespeare/model_1-2.h5')
+char_mapping = joblib.load('shakespeare/char_mapping.sav')
+ohe = joblib.load('shakespeare/ohe.sav')
+raw_text = open('shakespeare.txt').read().lower()
 n_vocab = len(char_mapping)
 
 # Create an inverse map of char mapping
